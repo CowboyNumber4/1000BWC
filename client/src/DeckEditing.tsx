26fa99f0d6c2d13283fb9659c89ed69dbefb4ea4
CowboyNumber4 <cowboynumberfour@gmail.com>
2025-01-13 15:04:14 +1030

    Update DeckEditing.tsx - final try "height"

--- client/src/DeckEditing.tsx
+++ client/src/DeckEditing.tsx
@@ -471,7 +471,7 @@ const styles: { [key: string]: React.CSSProperties } = {
     justifyContent: "center",
     minHeight: "calc(3/5 * 100vh)", // Set the minimum height to 3/5 of the screen height
     minWidth: "550px", // Set the minimum width to 550px
-    maxHeight: "100vh", // ===========================================================================================NEWLY ADDED
+    height: "100vh", // ===========================================================================================NEWLY ADDED
     margin: "calc(1/5 * 100vh) 0", // Set the top and bottom margin to 1/5 of the screen height
     backgroundColor: "#f0f0f0", // Off-white background
     textAlign: "center",
